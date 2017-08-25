48a0b6bf64c58ef1002cf0508507b4888c4533a6
alexhong742 <alexhong742@users.noreply.github.com>
2017-08-25 15:29:29 -0700

    recommit gian changs

--- server/server.js
+++ server/server.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-let express = require('express')
-let app = express()
-const path = require('path');
-const bodyParse = require('body-parser')
-// const fs = require('fs')
-
-app.use(bodyParse())
-app.use(express.static('build'))
-
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, './../index.html'))
-})
-
-app.get('/build/bundle.js', (req, res) => {
-    // res.sendFile(path.join(__dirname, './../build/bundle.js'))
-})
-
-// app.get('/download', () => {
-//     res.downloadFile(path.join(__dirname, './../store/'))
-// })
-
-app.listen(3000, () => {
-    console.log('now listening on 3000!');
-});
-=======
 let express = require('express');
 let app = express();
 const path = require('path');
@@ -50,4 +24,3 @@ app.get('/download', zipper, (req,res) => {
 app.listen(3000, () => {
     console.log('now listening on 3000!');
 }); 
->>>>>>> 186e6763858e72e7fc763b26f356eefdeaa57f3f

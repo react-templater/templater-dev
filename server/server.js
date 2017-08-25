ffe9d43b392141091a3f5ceffb6356162dd28a2a
alexhong742 <alexhong742@users.noreply.github.com>
2017-08-24 19:09:51 -0700

    server: added modules and handled download request

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-let express = require(express)
+let express = require('express')
 let app = express()
 const path = require('path');
 const bodyParse = require('body-parser')
+// const fs = require('fs')
 
 app.use(bodyParse())
 app.use(express.static('build'))
-
 
 app.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, './../index.html'))
@@ -15,6 +15,9 @@ app.get('/build/bundle.js', (req, res) => {
     // res.sendFile(path.join(__dirname, './../build/bundle.js'))
 })
 
+// app.get('/download', () => {
+//     res.downloadFile(path.join(__dirname, './../store/'))
+// })
 
 app.listen(3000, () => {
     console.log('now listening on 3000!');

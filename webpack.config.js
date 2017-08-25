9cb61a3d01eaa63e25397370d91174809cb2d9a6
alexhong742 <alexhong742@users.noreply.github.com>
2017-08-25 15:45:25 -0700

    revert json with image-webpack-loader

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,22 @@ module.exports = {
               name: 'assets/[name].[ext]'
             }
           },
-          
+           {		         
+            loader: 'image-webpack-loader',		
+            options: {		
+              query: {		
+                mozjpeg: {		
+                  progressive: true,		
+                },		
+                gifsicle: {		
+                  interlaced: true,		
+                },		
+                optipng: {		
+                  optimizationLevel: 7,		
+                },		
+              }		
+            },		
+          }
         ]
       }
     ],

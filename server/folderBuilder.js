99c782f869c4df6ab7abc07c363025c081194569
alexhong742 <alexhong742@users.noreply.github.com>
2017-08-25 16:57:19 -0700

    comit to pull from gian

--- server/folderBuilder.js
+++ server/folderBuilder.js
@@ -1,8 +1,22 @@
 const path = require('path')
 var copy = require('recursive-copy');
 module.exports = (req,res,next) => {
+
 let froms = path.join(__dirname, './../store/template')
 let tos = path.join(__dirname, './../store/polymerUI')
+function fromd(elem){
+    return path.join(__dirname, `./../store/template/${elem}`)
+}
+for(let w in req.body){
+    if(req.body[w] === 'assets'){
+        copy(fromd(w),tos, (err,result)=>{
+            if(err){console.log('Copy Failed: ' + err)}
+            else{console.info('Copied ' + result.length + ' files')}
+        })
+        console.log(fromd('w'))
+    }
+}
+if(!req.body.assets && !req.body.components && !req.body.build && req.body.style){
 copy(froms, tos, function(error, results) {
    if (error) {
        console.error('Copy failed: ' + error);
@@ -12,3 +26,22 @@ copy(froms, tos, function(error, results) {
    next()
 });
 }
+}
+// assets, auuthor, build, components, name, style
+
+
+// const path = require('path')
+// var copy = require('recursive-copy');
+// module.exports = (req,res,next) => {
+
+// let froms = path.join(__dirname, './../store/template')
+// let tos = path.join(__dirname, './../store/polymerUI')
+// copy(froms, tos, function(error, results) {
+//     if (error) {
+//         console.error('Copy failed: ' + error);
+//     } else {
+//         console.info('Copied ' + results.length + ' files');
+//     }
+//     next()
+//  });
+// }
--- server/server.js
+++ server/server.js
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 let express = require('express');
 let app = express();
 const path = require('path');
 const bodyParse = require('body-parser');
-let zipFolder = require('zip-folder');
 let zipper = require('./zipper');
 let createFolder = require('./folderBuilder.js');
 let remover = require('./remover.js');
-let findRemoveSync = require('find-remove');
 
 app.use(bodyParse());
 app.use(express.static('build'));
@@ -20,7 +17,7 @@ app.get('/build/bundle.js', (req, res) => {
     // res.sendFile(path.join(__dirname, './../build/bundle.js'));
 });
 
-app.get('/download', createFolder, zipper, (req,res) => {
+app.post('/download', createFolder, zipper, (req,res) => {
 // app.get('/', createFolder, zipper, (req,res) => {
     res.download(path.join(__dirname, './templateStore/zippedFile.zip'));
 });
@@ -28,29 +25,3 @@ app.get('/download', createFolder, zipper, (req,res) => {
 app.listen(3000, () => {
     console.log('now listening on 3000!');
 }); 
-=======
-let express = require('express')
-let app = express()
-const path = require('path');
-const bodyParse = require('body-parser')
-// const fs = require('fs')
-
-app.use(bodyParse())
-app.use(express.static('build'))
-
-app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, './../index.html'))
-})
-
-app.get('/build/bundle.js', (req, res) => {
-    // res.sendFile(path.join(__dirname, './../build/bundle.js'))
-})
-
-// app.get('/download', () => {
-//     res.downloadFile(path.join(__dirname, './../store/'))
-// })
-
-app.listen(3000, () => {
-    console.log('now listening on 3000!');
-});
->>>>>>> ffe9d43b392141091a3f5ceffb6356162dd28a2a

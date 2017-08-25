1ef4affc01be04cb6248e4f7dc4d71b21a8fd7b8
Jon Roach <roach.jc@gmail.com>
2017-08-24 21:27:24 -0700

    create user package.json and webpack.config

---
+++ store/template/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'client'),
+  entry: {
+    app: ['./index.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/assets/',
+    filename: 'bundle.js',
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\. $/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};

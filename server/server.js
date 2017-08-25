7d37ea4a352883dd7e535faf18424d76e82e9dee
GianIsAlive <gianthedesigner@gmail.com>
2017-08-25 16:17:17 -0700

    fixing clientUI bug

--- server/server.js
+++ server/server.js
@@ -1,23 +1,24 @@
-let express = require('express')
-let app = express()
+let express = require('express');
+let app = express();
 const path = require('path');
-const bodyParse = require('body-parser')
-// const fs = require('fs')
+const bodyParse = require('body-parser');
 
-app.use(bodyParse())
-app.use(express.static('build'))
+app.use(bodyParse.json());
 
 app.get("/", (req,res) => {
-    res.sendFile(path.join(__dirname, './../index.html'))
-})
+  res.sendFile(path.join(__dirname, '../index.html'));
+});
 
 app.get('/build/bundle.js', (req, res) => {
-    // res.sendFile(path.join(__dirname, './../build/bundle.js'))
-})
+  res.sendFile(path.join(__dirname, '../build/bundle.js'));
+});
 
-app.get('/download', () => {
-    res.downloadFile(path.join(__dirname, './../store/template/index.html'))
-})
+// app.get('/download', () => {
+  //res.downloadFile(path.join(__dirname, './../store/template/index.html'))
+  // });
+  
+app.use('/', express.static('../build'));
+// app.use('/build', express.static('../build/assets'));
 
 app.listen(3000, () => {
     console.log('now listening on 3000!');

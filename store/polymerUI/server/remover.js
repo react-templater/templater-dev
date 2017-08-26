fb19513a2dd404d1491484e93f657e616db37f83
GianIsAlive <gianthedesigner@gmail.com>
2017-08-26 15:18:39 -0700

    remember delete polymer UI

--- store/polymerUI/server/remover.js
+++
@@ -1,8 +0,0 @@
-let findRemoveSync = require('find-remove')
-let path = require('path')
-
-module.exports = (req,res,next) => {
-    let variable = findRemoveSync(path.join(__dirname, './../store/polymerUI'), {dir: "*", files: "*.*"})
-}
-
-

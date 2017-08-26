28edeac17edd4d874b2a505d61e66ba59e4c7932
alexhong742 <alexhong742@users.noreply.github.com>
2017-08-26 11:51:39 -0700

    server and controller updates

---
+++ server/moduleController.js
@@ -0,0 +1,85 @@
+const path = require('path')
+var copy = require('recursive-copy');
+
+const src = (include) => {
+  return path.join(__dirname, `./../store/options/${include}`)
+}
+
+module.exports = {
+
+  assets(req, res, next) {
+    if (!req.body.assets) next();
+    else{
+    const assetsDir = path.join(__dirname, './../store/polymerUI/client/assets/');
+    copy(src('assets'), assetsDir, (err,result) => {
+      if(err){console.log('Copy Failed: ' + err)}
+      else{console.info('Copied ' + result.length + ' asset files')}
+      next()
+    });
+  }
+  },
+
+  components(req, res, next) {
+    if (!req.body.components) {
+      next();
+    } else {
+      const componentsDir  = path.join(__dirname, './../store/polymerUI/client/components/');
+      copy(src('components'), componentsDir, (err,result) => {
+        if(err){console.log('Copy Failed: ' + err)}
+        else{console.info('Copied ' + result.length + ' component files')}
+        next()
+      });
+    };
+  },
+
+  style(req, res, next) {
+    if (!req.body.style) {
+      next();
+    } else {
+      const styleDir  = path.join(__dirname, './../store/polymerUI/client/style/');
+      copy(src('style'), styleDir, (err,result) => {
+        if(err){console.log('Copy Failed: ' + err)}
+        else{console.info('Copied ' + result.length + ' style files')}
+        next()
+      });
+    };
+  },
+}
+
+
+// (req,res,next) => {
+
+//   let assetsDir = path.join(__dirname, './../store/polymerUI/client/assets/')
+//   let componentsDir = path.join(__dirname, './../store/polymerUI/client/components/')
+//   let stylesDir = path.join(__dirname, './../store/polymerUI/client/styles/')
+//   function fromd(elem){
+//     return path.join(__dirname, `./../store/options/${elem}`)
+//   }
+
+//   if(req.body.assets){
+//     copy(fromd('assets'), assetsDir, (err,result)=>{
+//         console.log(fromd("assets"), 'Assets have been copied')
+//         if(err){console.log('Copy Failed: ' + err)}
+//         else{console.info('Copied ' + result.length + ' files')}
+//         next()
+//     })
+//   }
+//   // if(req.body.components){
+//   //   copy(fromd('components'), componentsDir, (err,result)=>{
+//   //       console.log(fromd("components"), 'Components have been copied')
+//   //       if(err){console.log('Copy Failed: ' + err)}
+//   //       else{console.info('Copied ' + result.length + ' files')}
+//   //       next()
+//   //   })
+//   // }
+//   // if(req.body.styles){
+//   //   copy(fromd('styles'), stylesDir, (err,result)=>{
+//   //       console.log(fromd("assets"), 'Styles have been copied')
+//   //       if(err){console.log('Copy Failed: ' + err)}
+//   //       else{console.info('Copied ' + result.length + ' files')}
+//   //       next()
+//   //   })
+//   // }
+
+
+// }

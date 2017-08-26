ce1fd54cae25f100d898e65bce7641eab3b8818c
GianIsAlive <gianthedesigner@gmail.com>
2017-08-26 14:34:15 -0700

    modified user UI content

--- store/options/components/Main.jsx
+++ store/options/components/Main.jsx
@@ -3,20 +3,28 @@ import { Component } from 'react';
 
 /*** Components ***/
 import Form from './Form.jsx';
+import Div from './Div.jsx';
+import PrimaryMessage from './PrimaryMessage.jsx';
+import SecondaryMessage from './SecondaryMessage.jsx';
+import Button from './Button.jsx';
 
 class Main extends Component {
   render() {
     return (
       <main>
-        <Form 
-          Name={this.props.name}
-          Author={this.props.author}
-          assets={this.props.assets}
-          components={this.props.components}
-          style={this.props.style}
-          build={this.props.build}
-          handleFormChange={this.props.handleFormChange}
-          clickDownload={this.props.clickDownload}/>
+        <Div 
+        className="main-image"/>
+        <PrimaryMessage 
+          className="primary-message"
+          PrimaryMessage="Create beautiful React App fast and furious"/>
+        <SecondaryMessage
+          className="secondary-message"
+          SecondaryMessage="A set of customizable React templates that helps developers to create beautiful App with few clicks"/>
+        <Button 
+          type="submit"
+          className="cta"
+          buttonName="Download Template"
+          click={this.props.toggleModal}/>
       </main>
     );
   }

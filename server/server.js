33eaccffabc0856626521ffb9d07adf208095d85
roachjc <roach.jc@gmail.com>
2017-08-25 17:04:48 -0700

    Cleaned up git lines

--- server/server.js
+++ server/server.js
@@ -2,10 +2,6 @@ let express = require('express');
 let app = express();
 const path = require('path');
 const bodyParse = require('body-parser');
-<<<<<<< HEAD
-=======
-let zipFolder = require('zip-folder');
->>>>>>> 9454ce8f36512ae6ad331728d05fdbea5a19ca04
 let zipper = require('./zipper');
 let createFolder = require('./folderBuilder.js');
 let remover = require('./remover.js');
@@ -22,14 +18,9 @@ app.get('/build/bundle.js', (req, res) => {
   res.sendFile(path.join(__dirname, '../build/bundle.js'));
 });
 
-<<<<<<< HEAD
 app.post('/download', createFolder, zipper, (req,res) => {
 // app.get('/', createFolder, zipper, (req,res) => {
     res.download(path.join(__dirname, './templateStore/zippedFile.zip'));
-=======
-app.get('/download', zipper, (req,res) => {
-  res.download(path.join(__dirname, './templateStore/zippedFile.zip'));
->>>>>>> 9454ce8f36512ae6ad331728d05fdbea5a19ca04
 });
 
 
